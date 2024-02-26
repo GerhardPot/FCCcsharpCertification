1e80487ee9f9b21ac0e616c30427c02c718f1db4
Gerhard <gerhardjpotgieter@gmail.com>
2024-02-26 13:24:38 +0200

    Created the first course

---
+++ Course1-2/Program.cs
@@ -0,0 +1,7 @@
+﻿string firstName = "Bob";
+int messageCount = 3;
+float temperature = 34.4F;
+
+Console.Write("Hello, " + firstName + "! ");
+Console.Write("You have " + messageCount + " messages in your inbox. ");
+Console.Write("The temperature is " + temperature + " celsius.");
---
+++ Course1-4/Program.cs
@@ -0,0 +1,5 @@
+﻿int fahrenheit = 94;
+
+double celsius = (fahrenheit - 32) * (5.0 / 9.0);
+
+Console.WriteLine($"The temperature is {celsius} Celsius.");
