c6e4dcbae53c3a12ff192e51ddacae761c704e8d
GerhardPot <gerhardjpotgieter@gmail.com>
2024-03-14 11:11:24 +0200

    Progress made.

---
+++ Courses/Course2/Course2-1/EU4/Program.cs
@@ -0,0 +1,8 @@
+﻿Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine(roll1);
+Console.WriteLine(roll2);
+Console.WriteLine(roll3);
