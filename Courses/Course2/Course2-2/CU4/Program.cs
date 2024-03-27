84e65b24f0a16aaf4979299137843947443fc487
GerhardPot <gerhardjpotgieter@gmail.com>
2024-03-28 00:43:19 +0200

    Made progress

---
+++ Courses/Course2/Course2-2/CU4/Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+string message = "";
+
+if (daysUntilExpiration == 0)
+{
+    message = "Your subscription has expired.";
+}
+
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    message = $"Your subscription expires within a day.\nRenew now and save {discountPercentage}%!";
+}
+
+else if (daysUntilExpiration <= 10)
+{
+    discountPercentage = 10;
+    message = $"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!";
+}
+
+else if (daysUntilExpiration <= 10)
+{
+    message = "Your subscription will expire soon. Renew now!";
+}
+
+Console.WriteLine(message);
